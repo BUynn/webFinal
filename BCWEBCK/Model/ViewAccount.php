f6c676ade266ab061eb350194f2f2306922f3990
dphuytdt <dphuytdt@gmail.com>
2022-05-21 04:45:24 +0700

    view account

---
+++ BCWEBCK/Model/ViewAccount.php
@@ -0,0 +1,20 @@
+<?php
+    require_once 'Config.php';
+    ob_start();
+    session_start();
+    //view account
+    if(isset($_POST['view'])){
+        $username = $_POST['username'];
+        $sql = "SELECT * FROM __account WHERE username = '$username'";
+        $result = mysqli_query($conn,$sql);
+        if(mysqli_num_rows($result) > 0){
+            while($row = mysqli_fetch_assoc($result)){
+                echo "Username: ".$row['username']."<br>";
+                echo "Password: ".$row['password']."<br>";
+            }
+        }else{
+            echo "Account not found";
+        }
+    }
+
+?>
