ecc3e09d977d01230201050d2ac9ae187026231f
dphuytdt <dphuytdt@gmail.com>
2022-05-21 16:52:17 +0700

    change password first time

--- BCWEBCK/Controller/CheckLogin.php
+++ BCWEBCK/Controller/CheckLogin.php
@@ -11,8 +11,15 @@
         $sql = "SELECT * FROM __account WHERE username = '$username' AND password = '$password'";
         $result = mysqli_query($conn,$sql);
         if(mysqli_num_rows($result) > 0){
-            $_SESSION['username'] = $username;
-            header("Location: ../View/home.php");
+            //check if status = 0 move to change password
+            $row = mysqli_fetch_assoc($result);
+            if($row['status'] == 0){
+                $_SESSION['username'] = $username;
+                header('Location: ../View/changePassword.php');
+            }else{
+                $_SESSION['username'] = $username;
+                header('Location: ../View/home.php');
+            }
         }else{
            //set text to html wrong username or password
             echo "Wrong username or password";
