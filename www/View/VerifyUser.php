fdfe77018ae0a5632d02b4a98718c9dda57631dd
Hi Truong <hitruong890@gmail.com>
2022-06-03 20:49:43 +0700

    webfinal

--- www/View/VerifyUser.php
+++ www/View/VerifyUser.php
@@ -1,16 +1,28 @@
-
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Enter Username</title>
+    <title>Forgot password</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
-    <h1>Enter Username</h1>
-    <form action="../Controller/CheckUser.php" method="post">
-        <label for="email">email</label>
-        <input type="text" name="email" id="email" required>
-        <input type="submit" name="verified" value="verified">
-        <button type="button" onclick="window.location.href='../View/login.php'">Cancel</button>
-    </form>
+    <div class="container col-3 bg-light">
+        <h2 class="row justify-content-center mt-3">Reset your password</h2>
+        <form action="../Controller/CheckUser.php" method="post">
+            <div class="mb-3 mt-3">
+                <label for="email">Email</label>
+                <input class="form-control" type="text" name="email" id="email" placeholder="Enter your email address..."
+                    required>
+            </div>
+            <div class="mb-3 mt-3 btn-group">
+                <input class="btn btn-outline-primary" type="submit" name="verified" value="Verified">
+                <button class="btn btn-outline-danger" type="button"
+                    onclick="window.location.href='../View/login.php'">Cancel</button>
+            </div>
+        </form>
+    </div>
 </body>
+
 </html>
--- www/View/__changePWD.php
+++ www/View/__changePWD.php
@@ -6,18 +6,32 @@
 
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Change Password</title>
+    <title>Change Your Password</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
-    <h1>Change Password</h1>
-    <form action="../Controller/__ChangePWD.php" method="post">
-        
-        <label for="new_password">New Password</label>
-        <input type="password" name="new_password" id="new_password" required>
-        <input type="password" name="confirm_password" id="confirm_password" required>
-        <input type="submit" name="change_password" value="change_password">
-        <button type="button" onclick="window.location.href='../View/home.php'">Cancel</button>
-    </form>
+    <div class="container col-3">
+        <h2 class="row justify-content-center mt-3">Set Your New Password</h2>
+        <form action="../Controller/__ChangePWD.php" method="post">
+            <div class="mb-3 mt-3">
+                <label for="new_password">New Password</label>
+                <input class="form-control" type="password" name="new_password" id="new_password"
+                    placeholder="Enter your new password..." required>
+            </div>
+            <div class="mb-3 mt-3">
+                <label for="new_password">Re-enter your new password</label>
+                <input class="form-control" type="password" name="confirm_password" id="confirm_password" placeholder="Re-enter your new password..." required>
+            </div>
+            <div class="mb-3 mt-3 btn-group">
+                <input class="btn btn-outline-primary" type="submit" name="change_password" value="Change Password">
+                <button class="btn btn-outline-danger" type="button" onclick="window.location.href='../View/home.php'">Cancel</button>
+            </div>
+        </form>
+    </div>
 </body>
+
 </html>
